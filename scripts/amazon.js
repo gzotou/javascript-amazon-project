24afb47d9cd574f9c6487c83cbdbc2dfaa1d21e8
gzotou <zotou.georgia@gmail.com>
2026-04-08 11:56:20 +0300

    Make the cart quantuty interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -82,7 +82,13 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
       });
     }
 
+    let cartQuantity = 0;
 
-    console.log(cart)
+    cart.forEach((item) => {
+      cartQuantity += item.quantity;
+    });
+
+    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+
   });
 })
