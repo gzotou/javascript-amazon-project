fd0e42cdea24412041dfdec86e490ef74df45d61
gzotou <zotou.georgia@gmail.com>
2026-04-23 15:17:22 +0300

    Learn error handling

--- data/products.js
+++ data/products.js
@@ -100,6 +100,8 @@ export function loadProductsFetch() {
 
     console.log('load products');
 
+  }).catch((error) => {
+    console.log('Unexpected error, Please try again later');
   });
   return promise;
 }
@@ -123,10 +125,15 @@ export function loadProducts(fun) {
 
     fun();
   });
+
+  xhr.addEventListener('error', (error) => {
+    console.log('Unexpected error, Please try again later');
+  });
+
   xhr.open('GET', 'https://supersimplebackend.dev/products');
   xhr.send();
 }
-
+loadProducts();
 
 /*
 export const products = [
